8a99f32066c03936ce2edcd6d766996fdab88fc6
YoungYul <rladufbf@naver.com>
2023-03-22 23:46:59 +0900

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,11 @@ import java.util.*;
 public class Main {
 
 
-    public static void Solution(String s1, String s2){
-        HashMap<String, Integer> map = new HashMap<>();
-        for(char x : s1.toCharArray()){
-            map.put(x,map.getOrDefault(x,0)+1);
-        }
-        for(char x : s2.toCharArray()){
-            map.put(x,map.getOrDefault(x,0)+1);
-        }
-
-
-
-    }
-
-
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         String s2 = sc.nextLine();
-
-        Solution(s,s2);
-
 
     }
 
